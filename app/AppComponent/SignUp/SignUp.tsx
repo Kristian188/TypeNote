d13356eab4ec6c37d0c98f4a30b225b7c020d060
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-02-18 20:16:04 +0200

    add validation schema in sign in and sign up

--- app/AppComponent/SignUp/SignUp.tsx
+++ app/AppComponent/SignUp/SignUp.tsx
@@ -14,17 +14,61 @@ import Link from "next/link";
 import { AppLogo } from "../AppLogo";
 import EmailInput from "../EmailInput";
 import PasswordInput from "../PasswordInput";
+import {useForm, FormProvider} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import { useToast } from "@/hooks/use-toast"
+import z from "zod";
+import { authSchema, signUpSchema } from "../validationSchema"
 
 
-
+type SignUpFormData = z.infer<typeof signUpSchema>
 
 export default function SignUp() {
+const methods = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpSchema)
+});
+const { toast } = useToast();
+
+const onSubmit = (data: SignUpFormData) => {
+  console.log("Sign In Data:", data);
+  toast({ title: "Sign in successful!", description: "You have signed in."});
+};
+
+function handleErrorsToast() {
+  const { errors } = methods.formState;
+  
+  if(errors.email) {
+    toast({
+      title: "Validation Error",
+      description: errors.email.message?.toString(),
+      variant: "destructive"
+    });
+
+  }
+
+  if(errors.password) {
+    toast({
+      title: "Validation Error",
+      description: errors.password.message?.toString(),
+      variant: "destructive"
+    })
+  }
+  if(errors.confirmPassword) {
+    toast({
+      title: "Validation Error",
+      description: errors.confirmPassword.message?.toString(),
+      variant: "destructive"
+    })
+  }
+}
 
 
   return (
     <div>
       <AppLogo />
       <Card className="w-full max-w-sm py-2">
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(onSubmit, handleErrorsToast)}>
 
             <CardHeader>
               <CardTitle className="text-[22px] font-bold">Sign Up</CardTitle>
@@ -45,6 +89,8 @@ export default function SignUp() {
             </CardContent>
             <CardFooter>
             </CardFooter>
+            </form>
+            </FormProvider>
       </Card>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 
 const poppins = Poppins({
@@ -35,7 +36,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
       >
-        {children}
+        <Toaster />
+        <main>{children}</main>
       </body>
     </html>
   );
