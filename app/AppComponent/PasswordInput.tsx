583de66c1187bb025294954d3f07d12723cd1609
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-02-18 19:54:30 +0200

    use form context in password input

--- app/AppComponent/PasswordInput.tsx
+++ app/AppComponent/PasswordInput.tsx
@@ -2,18 +2,28 @@ import React from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { ErrorHoverCard } from "./ErrorHoverCard";
+import { useFormContext } from "react-hook-form";
 
 
 function PasswordInput({ name, label }: {name: string; label: string}) {
+    const {
+        register,
+        formState: { errors },
+    } = useFormContext();
+
+    const errorMessage = errors[name]?.message?.toString();
+    
     return (
         <div className="grid gap-2 relative">
             <Label htmlFor="password">{label}</Label>
             <Input 
             id={name}
+            {...register(name)}
             type="password"
-            placeholder="Your Password..."
+            required
+            placeholder={`Your ${label}...`}
             />
-            <ErrorHoverCard message="Invalid Password"></ErrorHoverCard>
+            {errorMessage &&  <ErrorHoverCard message={errorMessage} />}
         </div>
     );
 }
