23cff65293944238134948f493430a663224aad4
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-02-21 20:42:46 +0200

    to dos pages

---
+++ to-dos/page.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { LogoutButton } from "@/LogoutBtn";
+import { useUserStore } from "../stores/userStore";
+
+export default function Dashboard() {
+    const { setUser, user } = useUserStore();
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        const validateUser = async () => { 
+            try {
+            const response = await fetch("/api/validate-user");
+            const data = await response.json();
+
+            if(data.isAuthenticated) {
+                console.log(data)
+                setUser(data.user);
+                
+            } else {
+                router.push("/");
+            }
+            } catch (error) {
+                console.error("User validation error:", error);
+                router.push("/");
+            } finally {
+                setLoading(false);
+            }
+        }
+        validateUser();
+
+    }, []);
+
+    if(loading) {
+        return <div>Loading...</div>
+    }
+
+    if(!user) {
+        return null;
+    }
+
+    return (
+        <div>
+            <h1>Welcome, {user.email}</h1>
+            <LogoutButton />
+        </div>
+    )
+
+}
