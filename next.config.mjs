63d2d0df514f4299ad31931d7b18da5d24e4af18
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-02-21 18:17:08 +0200

    logout

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  serverExternalPackages: ["@node-rs/argon2"], // Moved here
+};
+
+export default nextConfig;
