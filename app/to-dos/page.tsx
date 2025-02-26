197e99ecfa3b2610abc067bba1bcfeb8c23e3629
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-02-26 17:54:26 +0200

    to do page

--- app/to-dos/page.tsx
+++ app/to-dos/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { LogoutButton } from "@/LogoutBtn";
 import { useUserStore } from "@/store/useUserStore"; 
+import { TaskHeader } from "./Components/TaskHeader/TaskHeader";
+import Stats from "./Components/Stats/Stats";
 export default function Dashboard() {
     const { setUser, user } = useUserStore();
     const [loading, setLoading] = useState(true);
@@ -31,7 +33,7 @@ export default function Dashboard() {
         }
         validateUser();
 
-    }, []);
+    }, [router]);
 
     if(loading) {
         return <div>Loading...</div>
@@ -42,9 +44,12 @@ export default function Dashboard() {
     }
 
     return (
-        <div>
-            <p>Welcome, {user.email}</p>
-            <LogoutButton />
+        <div className="min-h-screen border flex items-center w-full justify-center ">
+            <div className="w-[55%] h-[90%] border flex flex-col gap-2 bg-white shadow-md rounded-md p-7">
+                <TaskHeader />
+                <Stats />
+            </div>
+
         </div>
     )
 
