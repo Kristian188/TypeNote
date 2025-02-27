cf4a417190d9839b9627e98fcd0bb217a49996fd
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-02-27 22:10:49 +0200

    alert footer

---
+++ app/to-dos/Components/Dialogs/TaskDialog/TaskForm.tsx
@@ -0,0 +1,12 @@
+import { ClearAllDialog } from "../ClearAllDialog/ClearAllDialog";
+
+export function TaskFooter() {
+    return (
+        <div>
+            <div className="flex justify-between mt-5 items-center">
+                <p className="text-gray-500 text-sm">12 Tasks</p>
+                <ClearAllDialog />
+            </div>
+        </div>
+    )
+}
