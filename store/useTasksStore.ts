c7783329bcc154182812c6dde1d41d979dc3ee01
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-03-03 19:43:08 +0200

    useTasksStore

---
+++ store/useTasksStore.ts
@@ -0,0 +1,39 @@
+import {create} from 'zustand';
+import { allTasks, Task } from '@/app/data/Task';
+
+interface useTasksStoreInterface {
+    tasks: Task[];
+    setTasks: (tasks: Task[]) => void;
+    fetchTasks: () => void;
+    updateTaskfunction: (Task: Task) => void;
+    
+}
+
+export const useTasksStore = create<useTasksStoreInterface>((set, get) => {
+    return {
+        tasks: [],
+        setTasks: (tasks: Task[]) => set({ tasks: tasks }),
+        fetchTasks: async () => {
+            const fetchedTasks = await new Promise<Task[]>((resolve) => {
+                setTimeout(() => {
+                    resolve(allTasks);
+                }, 1000);
+            });
+            set({ tasks: fetchedTasks });
+        },
+
+        updateTaskfunction: (task: Task) => {
+            const currentTasks = get().tasks;
+            const updatedTasks: Task[] = currentTasks.map((t) => {
+                if(t.id === task.id) {
+                    return { ...t, ...task };
+                }
+                return t;
+
+            });
+            set({ tasks: updatedTasks });
+        }
+    };
+
+});
+            
